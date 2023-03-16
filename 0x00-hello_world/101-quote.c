42179c6a79835105e977f771a84552a3c243caa2
talenttinaapi <ttinaapi88@gmail.com>
2023-03-16 14:26:25 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 /**
@@ -7,7 +8,6 @@
  **/
 int main(void)
 {
-	write("1,
-		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	system("ech0 'and that piece of art is useful\" - Dora Korpar, 2015-10-19' ");
 	return	(1);
 }
