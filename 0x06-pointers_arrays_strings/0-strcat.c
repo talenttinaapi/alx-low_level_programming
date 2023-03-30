f33fa04c37e17d1159bba64c2a06fc070a5661c3
talenttinaapi <ttinaapi88@gmail.com>
2023-03-30 07:37:25 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_strcat - Concatenates twi strings
+ *@dest: The destination string
+ *@src: The source string
+ *Return: A pointer to the resluting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int dlen = 0, i;
+
+	while (dest[dlen])
+	{
+		dlen++;
+	}
+
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[dlen] = src[i];
+		dlen++;
+	}
+
+	dest[dlen] = '\0';
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Pointerss readme
