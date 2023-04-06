6c0bcab3640be4fd370ce3d26cc091187b0f3a03
talenttinaapi <ttinaapi88@gmail.com>
2023-04-06 13:12:12 -0700

    100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *_strlen_recursion - returns the length of a string
+ *@s: string
+ *Return: the length of a string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ *is_palindrome - detects if a string is a palindrome
+ *@s:string
+ *Return: 1 if is a palindrom, 0 if not
+ */
+int comparator(char *s, int n1, int n2)
+{
+	if (*(s + n1) == *(s + n2))
+	{
+		if (n1 == n2 || n1 == n2 + 1)
+			return (1);
+		return (0 + comparator(s, n1 + 1, n2 - 1));
+	}
+	return (0);
+}
