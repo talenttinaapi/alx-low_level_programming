0cac6cb7e75c1a91e6e7bd90d0b7660f7e04af35
talenttinaapi <ttinaapi88@gmail.com>
2023-04-06 11:24:57 -0700

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,19 +9,11 @@
  */
 int _pow_recursion(int x, int y)
 {
-	if (y < 0)
+	if (y <= 0)
 		return (-1);
 
 	if (y == 0)
 		return (1);
 
-	if (y == 1)
-		return (x);
-
-	int result = _pow_recursion(x, y / 2);
-
-	if (y % 2 == 0)
-		return (result * result);
-
-	return (result * result * x);
+	return (x * _pow_recursion(x, y - 1));
 }
