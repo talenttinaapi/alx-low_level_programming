cb03d325e47fdc74ea235f54fe2ddbe6f89d626b
talenttinaapi <ttinaapi88@gmail.com>
2023-04-10 14:53:13 -0700

    1-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+				
+/**
+ * main - prints all arguments it receives		
+ * @argc: number of arguments
+ * @argv: argument vector
+ *
+ * Return: Always 0 		
+ */
+		
+int main(int argc, char *argv[])
+{
+	int i;
+		
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}	
+	return (0);
+		
+}
+
