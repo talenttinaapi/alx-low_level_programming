e1d36af2cba23e8c1fc0830104e5d4c5025406e0
talenttinaapi <ttinaapi88@gmail.com>
2023-04-10 14:03:05 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,10 +9,9 @@
  *Return: Always 0
  */
 
-int main (int argc, char *argv[])
+int main(int argc_attribute_((unused)), char *argv[])
 {
-	(void) argc;
-	printf("%s\n", argv[0]);
+	printf("%s\n", *argv);
 
 	return (0);
 }
