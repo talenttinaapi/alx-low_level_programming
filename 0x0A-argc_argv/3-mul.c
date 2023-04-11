cea5766a401a49ce205ce43da36e7ab19fcef599
talenttinaapi <ttinaapi88@gmail.com>
2023-04-11 02:56:57 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,15 +11,19 @@
  */
 int main(int argc, char *argv[])
 {
+	int n1 = 0, n2 = 0;
 
-	if (argc != 3)
+	if (argc == 3)
 	{
-		fprintf(stderr, "Usage: %s number2\n", argv[0]);
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
+		printf("%d\n", n1 * n2);
+	}
+	else
+	{
+		printf("Error\n");
 		return (1);
 	}
-		int n1 = atoi(argv[1]);
-		int n2 = atoi(argv[2]);
 
-		printf("%d\n", n1 * n2);
-		return (0);
+	return (0);
 }
