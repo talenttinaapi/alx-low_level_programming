d84f167b4204da62f013861b31832e314b47adaf
talenttinaapi <ttinaapi88@gmail.com>
2023-04-12 06:03:41 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,8 +14,7 @@ char *str_concat(char *s1, char *s2)
 	int i = 0, j = 0, k = 0, l = 0;
 	char *s;
 
-
-	     if (s1 == NULL)
+		if (s1 == NULL)
 		s1 = "";
 
 	if (s2 == NULL)
@@ -24,7 +23,7 @@ char *str_concat(char *s1, char *s2)
 	while (s1[1])
 		i++;
 
-	  while (s2[j])
+	while (s2[j])
 		j++;
 
 	l = i + j;
