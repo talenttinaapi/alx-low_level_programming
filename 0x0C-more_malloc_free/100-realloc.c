e6ae1dff33a92d82a7b1916d1a86f0f443f00e31
talenttinaapi <ttinaapi88@gmail.com>
2023-04-14 03:33:01 -0700

    100-realloc.c

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *_realloc - ...
+ *@ptr: ...
+ *@old_size: ...
+ *@new_size: ...
+ *
+ *Return: ...
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+   char *p;
+   unsigned int i, n = new_size;
+   char *oldp = ptr;
+
+
+   if (ptr == NULL)
+   {
+       p = malloc(new_size);
+       return (p);
+   }
+   else if (new_size == 0)
+   {
+       free(ptr);
+       return (NULL);
+   }
+   else if (new_size == old_size)
+       return (ptr);
+   p = malloc(new_size);
+   if (p == NULL)
+	       return (NULL);
+   if (new_size > old_size)
+       n = old_size;
+   for (i = 0; i < n; i++)
+       p[i] = oldp[i];
+   free(ptr);
+   return (p);
+}
+
