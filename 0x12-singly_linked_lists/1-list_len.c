54fd045f67756a34ab36fd9c3aef79e7ca309519
talenttinaapi <ttinaapi88@gmail.com>
2023-04-30 11:10:33 -0700

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,18 +3,18 @@
 
 /**
  *list_len - returns num of elements in a linked list
- *@h: pointer to the list_t list
+ *@j: pointer to the list_t list
  *
  *Return: number of elements in h
  */
-size_t list_len(const list_t *h)
+size_t list_len(const list_t *j)
 {
 	size_t n = 0;
 
-	while (h)
+	while (j)
 	{
 	n++;
-	h = h->next;
+	j = j->next;
 	}
 	return (n);
 }
