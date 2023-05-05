c0098025ec9748e31149b23ef4851cad5fe32a81
talenttinaapi <ttinaapi88@gmail.com>
2023-05-05 02:23:25 -0700

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *print_binary - ...
+ *@n: ...
+ */
+void print_binary(unsigned long int n)
+{
+	int k, count = 0;
+	unsigned long int current;
+
+	for (k = 63; k >= 0; k--)
+	{
+		current = n >> k;
+
+		if (current & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
+}
